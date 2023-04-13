37ef9947cbd9bba0fb5eedaca29a2985474ab046
ANIKA Noshin Islam <noshin-islam.anika@cimpa.com>
2023-04-13 10:18:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+   //  document.write("this is a javascript file");
+//    var name = "Noshin";
+//    var age  = "26";
+//    document.write(name); 
+//    document.write(age);
+
+//    var name = "yamin";
+//    var age  = "29";
+//    document.write(name); 
+//    document.write(age);
+
+var firstname = "Noshin";
+var lastaname = " Islam";
+var fullname = firstname + lastaname;
+//document.write("My name is " + fullname);
+var age = 23
+document.write(fullname +" is my name and " + age + " is my age");
---
+++ script1.js
@@ -0,0 +1,13 @@
+//  var name = prompt ("Write your name : "); //taking data from you 
+// // document.write(name.length);
+// name = name.length;
+// document.write(name);
+var name = "Noshin";
+var name1 = "Anika";
+
+name = name.toLowerCase();
+name1 = name1.toUpperCase();
+
+
+document.write(name);
+document.write(name1);
